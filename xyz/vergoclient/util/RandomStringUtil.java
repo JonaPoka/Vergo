a85cf8f6320d2b54967b2d2aefe46153024ccb3a
Rismose <rasmus.h.e@hotmail.se>
2021-12-19 14:37:31 +0100

    More funny string

--- xyz/vergoclient/util/RandomStringUtil.java
+++ xyz/vergoclient/util/RandomStringUtil.java
@@ -11,7 +11,8 @@ public class RandomStringUtil {
 				"SOUNDPADS, SPEAKERS, ANTENNA!", "yo momma's fucking hot boi!", "Smocking with Snoop Dogg", "WatchDawg aint got shit on us", "Hypixel crying right now",
 				"LAUGH!", "RAID SHADOW LEGENDS!", "I hope you die of corona", "Stop watching me load!", "FUCK YOU BITCH", "Watching Boko No Piko", "Did you know Zebras don't wear condoms?", "Rismose is sussy",
 				"Vergo just casually fucking your mom", "Who's an edgy cheater? You are!", "Vergo giving you Stockholm syndrome", "Wonder how tear is coping with this", "Coin is sexy", "I like pancakes",
-				"Oh... Hello there", "To all our early supporters, <3", "Oh, you... didn't mean to load me right?", "No.");
+				"Oh... Hello there", "To all our early supporters, <3", "Oh, you... didn't mean to load me right?", "No.", "We love coiny", "QUACK!", "Meow?", "Imagine having friends...", "Åååååhhhh, jag kommer",
+				"Stefan Löfven är en heting geting", "Oh there you are!", "Oh no, not you again");
 		return theFunny.getRandomObject();
 	}
 	
