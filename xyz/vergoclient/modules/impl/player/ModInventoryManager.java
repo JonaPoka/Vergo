4ecb5394b67055e024e3bf0c262d86cb4b2a77dc
jiggey1 <fortniitetradezback@gmail.com>
2021-12-30 23:36:02 +0000

    stuff & InvManager improved by miles.

--- xyz/vergoclient/modules/impl/player/ModInventoryManager.java
+++ xyz/vergoclient/modules/impl/player/ModInventoryManager.java
@@ -6,11 +6,13 @@ import java.util.List;
 
 import com.google.common.collect.Lists;
 
+import net.minecraft.item.*;
 import xyz.vergoclient.Vergo;
 import xyz.vergoclient.event.Event;
 import xyz.vergoclient.event.impl.EventTick;
 import xyz.vergoclient.modules.Module;
 import xyz.vergoclient.modules.OnEventInterface;
+import xyz.vergoclient.settings.BooleanSetting;
 import xyz.vergoclient.settings.ModeSetting;
 import xyz.vergoclient.settings.NumberSetting;
 import xyz.vergoclient.util.InventoryUtils;
@@ -19,75 +21,70 @@ import net.minecraft.client.gui.inventory.GuiChest;
 import net.minecraft.client.gui.inventory.GuiContainerCreative;
 import net.minecraft.client.gui.inventory.GuiInventory;
 import net.minecraft.inventory.Slot;
-import net.minecraft.item.ItemArmor;
-import net.minecraft.item.ItemAxe;
-import net.minecraft.item.ItemHoe;
-import net.minecraft.item.ItemPickaxe;
-import net.minecraft.item.ItemSpade;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemSword;
 import net.minecraft.network.play.client.C0BPacketEntityAction;
 import net.minecraft.network.play.client.C0DPacketCloseWindow;
 import net.minecraft.network.play.client.C0BPacketEntityAction.Action;
 
-public class 	ModInventoryManager extends Module implements OnEventInterface{
+import static xyz.vergoclient.util.InventoryUtils.isBadPotion;
+
+public class 	ModInventoryManager extends Module implements OnEventInterface {
 
 	public ModInventoryManager() {
 		super("InventoryManager", Category.PLAYER);
 	}
-	
+
 	public static transient boolean hasMovedItem = false;
-	
+
 	public ModeSetting mode = new ModeSetting("Mode", "Open inv", "Silent", "Open inv");
 	public NumberSetting tickDelay = new NumberSetting("Tick delay", 1, 1, 10, 1);
-	
+
+	public static BooleanSetting dropBowAndArrows = new BooleanSetting("Drop BowAndArrows?", false);
+
 	@Override
 	public void loadSettings() {
 		mode.modes.clear();
 		mode.modes.addAll(Arrays.asList("Silent", "Open inv"));
-		addSettings(tickDelay, mode);
+		addSettings(dropBowAndArrows, tickDelay, mode);
 	}
-	
+
 	@Override
 	public void onEnable() {
 		hasMovedItem = false;
 	}
-	
+
 	@Override
 	public void onDisable() {
 		hasMovedItem = false;
 	}
-	
+
 	@Override
 	public void onEvent(Event e) {
-		
+
 		if (e instanceof EventTick && e.isPost()) {
 			setInfo("Hypixel");
 
 			if (mc.thePlayer.ticksExisted % 60 == 0) {
 //				mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0DPacketCloseWindow(mc.thePlayer.inventoryContainer.windowId));
 			}
-		}
-		else if (e instanceof EventTick && e.isPre() && mc.thePlayer.ticksExisted % tickDelay.getValueAsInt() == 0) {
-			
+		} else if (e instanceof EventTick && e.isPre() && mc.thePlayer.ticksExisted % tickDelay.getValueAsInt() == 0) {
+
 			if (!mode.is("Silent") || !MovementUtils.isMoving()) {
 				moveOrDropLoop(e);
-			}
-			else if (mode.is("Silent") && hasMovedItem) {
+			} else if (mode.is("Silent") && hasMovedItem) {
 				mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0DPacketCloseWindow(mc.thePlayer.inventoryContainer.windowId));
 				hasMovedItem = false;
 			}
-			
+
 		}
-		
+
 	}
-	
+
 	private void moveOrDropLoop(Event e) {
 		if (e instanceof EventTick && e.isPre()) {
-			
+
 //			if ((mc.currentScreen == null || (!(mc.currentScreen instanceof GuiChest)) && !(mc.currentScreen instanceof GuiContainerCreative)) && mc.thePlayer.ticksExisted % 4 == 0) {
 			if (mode.is("Silent") ? ((mc.currentScreen == null || (!(mc.currentScreen instanceof GuiChest)) && !(mc.currentScreen instanceof GuiContainerCreative))) : (mc.currentScreen != null && mc.currentScreen instanceof GuiInventory)) {
-				
+
 				List<Slot> inventorySlots = Lists.<Slot>newArrayList();
 				inventorySlots.addAll(mc.thePlayer.inventoryContainer.inventorySlots);
 				Collections.shuffle(inventorySlots);
@@ -104,33 +101,32 @@ public class 	ModInventoryManager extends Module implements OnEventInterface{
 							}
 							return;
 						}
-						
+
 					}
-					
+
 				}
-				
+
 				if (mode.is("Silent") && hasMovedItem) {
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0DPacketCloseWindow(mc.thePlayer.inventoryContainer.windowId));
 					hasMovedItem = false;
 				}
-				
-			}
-			else if (mode.is("Silent") && hasMovedItem && MovementUtils.isMoving()) {
+
+			} else if (mode.is("Silent") && hasMovedItem && MovementUtils.isMoving()) {
 //				mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0DPacketCloseWindow(mc.thePlayer.inventoryContainer.windowId));
 				hasMovedItem = false;
 			}
 		}
 	}
-	
+
 	private static int swordSlot = 0, pickaxeSlot = 1, axeSlot = 2;
-	
+
 	private static boolean moveOrDropItem(Slot slot, int windowId) {
-		
+
 		ItemStack stack = slot.getStack();
-		
+
 		if (stack == null || stack.getItem() == null)
 			return false;
-		
+
 		if (stack.getItem() instanceof ItemSword) {
 			if (InventoryUtils.getBestSwordSlot() != null && InventoryUtils.getBestSwordSlot() == slot) {
 				if (slot.slotNumber == swordSlot)
@@ -139,15 +135,14 @@ public class 	ModInventoryManager extends Module implements OnEventInterface{
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.swap(slot.slotNumber, swordSlot, windowId);
-			}else {
+			} else {
 				if (!hasMovedItem && Vergo.config.modInventoryManager.mode.is("Silent")) {
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.drop(slot.slotNumber, windowId);
 			}
 			return true;
-		}
-		else if (stack.getItem() instanceof ItemPickaxe) {
+		} else if (stack.getItem() instanceof ItemPickaxe) {
 			if (InventoryUtils.getBestPickaxeSlot() != null && InventoryUtils.getBestPickaxeSlot() == slot) {
 				if (slot.slotNumber == pickaxeSlot)
 					return false;
@@ -155,15 +150,14 @@ public class 	ModInventoryManager extends Module implements OnEventInterface{
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.swap(slot.slotNumber, pickaxeSlot, windowId);
-			}else {
+			} else {
 				if (!hasMovedItem && Vergo.config.modInventoryManager.mode.is("Silent")) {
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.drop(slot.slotNumber, windowId);
 			}
 			return true;
-		}
-		else if (stack.getItem() instanceof ItemAxe) {
+		} else if (stack.getItem() instanceof ItemAxe) {
 			if (InventoryUtils.getBestAxeSlot() != null && InventoryUtils.getBestAxeSlot() == slot) {
 				if (slot.slotNumber == axeSlot)
 					return false;
@@ -171,43 +165,94 @@ public class 	ModInventoryManager extends Module implements OnEventInterface{
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.swap(slot.slotNumber, axeSlot, windowId);
-			}else {
+			} else {
 				if (!hasMovedItem && Vergo.config.modInventoryManager.mode.is("Silent")) {
 					mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 				}
 				InventoryUtils.drop(slot.slotNumber, windowId);
 			}
 			return true;
-		}
-		else if (stack.getItem() instanceof ItemSpade || stack.getItem() instanceof ItemHoe) {
+		} else if (stack.getItem() instanceof ItemSpade || stack.getItem() instanceof ItemHoe) {
 			if (!hasMovedItem && Vergo.config.modInventoryManager.mode.is("Silent")) {
 				mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 			}
 			InventoryUtils.drop(slot.slotNumber, windowId);
 			return true;
-		}
-		else if (stack.getItem() instanceof ItemArmor) {
-			
+		} else if (stack.getItem() instanceof ItemArmor) {
+
 			// Those slot numbers are for the currently equipped armor
 			if (slot.slotNumber >= 5 && slot.slotNumber <= 8)
 				return false;
-			
+
 			for (int type = 1; type < 5; type++) {
 				ItemStack currentArmor = mc.thePlayer.inventoryContainer.getSlot(4 + type).getStack();
 				if (ModAutoArmor.isBestArmor(stack, type) && (currentArmor == null || !ModAutoArmor.isBestArmor(currentArmor, type))) {
 					return false;
 				}
 			}
-			
+
 			if (!hasMovedItem && Vergo.config.modInventoryManager.mode.is("Silent")) {
 				mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C0BPacketEntityAction(mc.thePlayer, Action.OPEN_INVENTORY));
 			}
 			InventoryUtils.drop(slot.slotNumber, windowId);
 			return true;
+		} else {
+			if (isTrash(stack)) {
+
+				InventoryUtils.drop(slot.slotNumber, windowId);
+			}
 		}
-		
+
 		return false;
-		
+
+	}
+
+	public static boolean isTrash(ItemStack item) {
+
+		if (dropBowAndArrows.isDisabled()) {
+			if (item.getItem().getUnlocalizedName().contains("bow") || (item.getItem().getUnlocalizedName().contains("arrow"))) {
+				return ((item.getItem().getUnlocalizedName().contains("tnt")) || item.getDisplayName().contains("Frog") ||
+						(item.getItem().getUnlocalizedName().contains("stick")) ||
+						(item.getItem().getUnlocalizedName().contains("string")) || (item.getItem().getUnlocalizedName().contains("flint")) ||
+						(item.getItem().getUnlocalizedName().contains("feather")) || (item.getItem().getUnlocalizedName().contains("bucket")) ||
+						(item.getItem().getUnlocalizedName().contains("snow")) || (item.getItem().getUnlocalizedName().contains("enchant")) ||
+						(item.getItem().getUnlocalizedName().contains("exp")) || (item.getItem().getUnlocalizedName().contains("shears")) || (item.getItem().getUnlocalizedName().contains("anvil")) ||
+						(item.getItem().getUnlocalizedName().contains("torch")) || (item.getItem().getUnlocalizedName().contains("seeds")) ||
+						(item.getItem().getUnlocalizedName().contains("leather")) || (item.getItem().getUnlocalizedName().contains("boat")) ||
+						(item.getItem().getUnlocalizedName().contains("fishing")) || (item.getItem().getUnlocalizedName().contains("wheat")) ||
+						(item.getItem().getUnlocalizedName().contains("flower")) || (item.getItem().getUnlocalizedName().contains("record")) ||
+						(item.getItem().getUnlocalizedName().contains("note")) || (item.getItem().getUnlocalizedName().contains("sugar")) ||
+						(item.getItem().getUnlocalizedName().contains("wire")) || (item.getItem().getUnlocalizedName().contains("trip")) ||
+						(item.getItem().getUnlocalizedName().contains("slime")) || (item.getItem().getUnlocalizedName().contains("web")) ||
+						((item.getItem() instanceof ItemGlassBottle)) || (item.getItem().getUnlocalizedName().contains("piston")) ||
+						(item.getItem().getUnlocalizedName().contains("potion") && (isBadPotion(item))) ||
+						//   ((item.getItem() instanceof ItemArmor) && isBestArmor(item)) ||
+						(item.getItem() instanceof ItemEgg && !item.getDisplayName().contains("Kit")) ||
+						//   ((item.getItem() instanceof ItemSword) && !isBestSword(item)) ||
+						(item.getItem().getUnlocalizedName().contains("Raw")));
+			}
+
+		}
+
+			return ((item.getItem().getUnlocalizedName().contains("tnt")) || item.getDisplayName().contains("Frog") ||
+					(item.getItem().getUnlocalizedName().contains("stick")) ||
+					(item.getItem().getUnlocalizedName().contains("string")) || (item.getItem().getUnlocalizedName().contains("flint")) ||
+					(item.getItem().getUnlocalizedName().contains("feather")) || (item.getItem().getUnlocalizedName().contains("bucket")) ||
+					(item.getItem().getUnlocalizedName().contains("snow")) || (item.getItem().getUnlocalizedName().contains("enchant")) ||
+					(item.getItem().getUnlocalizedName().contains("exp")) || (item.getItem().getUnlocalizedName().contains("shears")) || (item.getItem().getUnlocalizedName().contains("anvil")) ||
+					(item.getItem().getUnlocalizedName().contains("torch")) || (item.getItem().getUnlocalizedName().contains("seeds")) ||
+					(item.getItem().getUnlocalizedName().contains("leather")) || (item.getItem().getUnlocalizedName().contains("boat")) ||
+					(item.getItem().getUnlocalizedName().contains("fishing")) || (item.getItem().getUnlocalizedName().contains("wheat")) ||
+					(item.getItem().getUnlocalizedName().contains("flower")) || (item.getItem().getUnlocalizedName().contains("record")) ||
+					(item.getItem().getUnlocalizedName().contains("note")) || (item.getItem().getUnlocalizedName().contains("sugar")) ||
+					(item.getItem().getUnlocalizedName().contains("wire")) || (item.getItem().getUnlocalizedName().contains("trip")) ||
+					(item.getItem().getUnlocalizedName().contains("slime")) || (item.getItem().getUnlocalizedName().contains("web")) ||
+					((item.getItem() instanceof ItemGlassBottle)) || (item.getItem().getUnlocalizedName().contains("piston")) ||
+					(item.getItem().getUnlocalizedName().contains("potion") && (isBadPotion(item))) || (item.getItem().getUnlocalizedName().contains("arrow")) ||
+					//   ((item.getItem() instanceof ItemArmor) && isBestArmor(item)) ||
+					(item.getItem() instanceof ItemEgg  || item.getItem().getUnlocalizedName().contains("bow") && !item.getDisplayName().contains("Kit")) ||
+					//   ((item.getItem() instanceof ItemSword) && !isBestSword(item)) ||
+					(item.getItem().getUnlocalizedName().contains("Raw")));
 	}
 	
 }
