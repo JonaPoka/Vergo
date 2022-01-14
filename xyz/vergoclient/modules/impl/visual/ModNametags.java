8f1b78f71d1de28fe0943f759e77400a5b04f2b7
jiggey1 <fortniitetradezback@gmail.com>
2022-01-14 09:05:23 +0000

    FLUX NAMETAGS <3

--- xyz/vergoclient/modules/impl/visual/ModNametags.java
+++ xyz/vergoclient/modules/impl/visual/ModNametags.java
@@ -9,9 +9,12 @@ import xyz.vergoclient.event.impl.EventRender3D;
 import xyz.vergoclient.modules.Module;
 import xyz.vergoclient.modules.OnEventInterface;
 import xyz.vergoclient.modules.impl.miscellaneous.ModAntiBot;
+import xyz.vergoclient.settings.ModeSetting;
 import xyz.vergoclient.ui.fonts.FontUtil;
 import xyz.vergoclient.ui.fonts.JelloFontRenderer;
 import xyz.vergoclient.util.MiscellaneousUtils;
+import xyz.vergoclient.util.RenderUtils;
+import xyz.vergoclient.util.RenderUtils2;
 import xyz.vergoclient.util.RotationUtils;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
@@ -19,12 +22,25 @@ import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.Vec3;
 
+import java.awt.*;
+import java.util.Arrays;
+
 import static xyz.vergoclient.modules.impl.visual.ModTargetHud.healthBar;
 
 public class ModNametags extends Module implements OnEventInterface {
 
 	public ModNametags() {
 		super("Nametags", Category.VISUAL);
+	}
+
+	public ModeSetting mode = new ModeSetting("Design", "Flux", "Flux");
+
+	@Override
+	public void loadSettings() {
+
+		mode.modes.addAll(Arrays.asList("Flux"));
+
+		addSettings(mode);
 	}
 
 	@Override
@@ -40,7 +56,7 @@ public class ModNametags extends Module implements OnEventInterface {
 				}
 				Vec3 targetPos = MiscellaneousUtils.getRenderEntityPos(player);
 				double x = targetPos.xCoord, y = targetPos.yCoord + player.getEyeHeight() + 1, z = targetPos.zCoord;
-				String text = "§f" + player.getDisplayName().getUnformattedTextForChat();
+				String text = "§f" + player.getName();
 				String scoreTitle = text;
 				String textNoColorFormatting = "";
 				boolean removeNext = false;
@@ -72,8 +88,8 @@ public class ModNametags extends Module implements OnEventInterface {
 				GlStateManager.enableTexture2D();
 				GlStateManager.translate((x - mc.getRenderManager().renderPosX) * scale,
 						(y - mc.getRenderManager().renderPosY) * -scale, (z - mc.getRenderManager().renderPosZ) * -scale);
-				GlStateManager.rotate((float) rots[0], 0, 1, 0);
-				GlStateManager.rotate((float) rots[1], 1, 0, 0);
+				GlStateManager.rotate(rots[0], 0, 1, 0);
+				GlStateManager.rotate(rots[1], 1, 0, 0);
 				JelloFontRenderer fr = FontUtil.juraNormal;
 				GlStateManager.rotate(180, 0, 1, 0);
 				GlStateManager.color(1, 1, 1, 1);
@@ -82,27 +98,28 @@ public class ModNametags extends Module implements OnEventInterface {
 				
 				// Render stuff
 				// Tinted background
-				Gui.drawRect(-3, -3, fr.getStringWidth(text) + 3, fr.FONT_HEIGHT + 3, 0x90000000);
-				
-				// Heath bar
-				double health = (fr.getStringWidth(text) + 6) * (player.getHealth() / player.getMaxHealth());
+				RenderUtils2.drawBorderedRect(-4, 0, (float) fr.getStringWidth(text) + 28, 15, 0.3f, new Color(0xD2212121, true), new Color(0xCD0A0A0A, true));
 
-				Gui.drawRect(-3, fr.FONT_HEIGHT, fr.getStringWidth(text) + 3, fr.FONT_HEIGHT + 3, 0x90000000);
-				Gui.drawRect(-3, fr.FONT_HEIGHT, health - 3, fr.FONT_HEIGHT + 3, Colors.BLUE.getColor());
 				
 				// Render name
 				GlStateManager.translate(0, 1, 0);
-//				GlStateManager.colorState.alpha = 1;
-//				fr.drawString(textNoColorFormatting, -0.1F, 0.0F, -1);
-//				GlStateManager.colorState.alpha = 1;
-//				fr.drawString(textNoColorFormatting, 0.0F, -0.1F, -1);
-//				GlStateManager.colorState.alpha = 1;
-//				fr.drawString(textNoColorFormatting, 0.1F, 0.0F, -1);
-//				GlStateManager.colorState.alpha = 1;
-//				fr.drawString(textNoColorFormatting, 0.0F, 0.1F, -1);
 				GlStateManager.colorState.alpha = 1;
-				fr.drawString(text, 0, 0, color);
-				
+				fr.drawString(text, 0, 3, color);
+
+				String healthStr = Math.round(player.getHealth() * 20) / 20d + "";
+
+				Color healthColor = new Color(95, 255, 67);
+
+				if(player.getHealth() >= 15) {
+					healthColor = new Color(95, 255, 67);
+				} else if(player.getHealth() <= 8) {
+					healthColor = new Color(255, 108, 51);
+				} else if(player.getHealth() <= 3) {
+					healthColor = new Color(253, 40, 40);
+				}
+
+				FontUtil.comfortaaSmall.drawString(healthStr, fr.getStringWidth(text) + 7, 5.5f, healthColor.getRGB());
+
 				GlStateManager.color(1, 1, 1, 1);
 				GlStateManager.disableTexture2D();
 				GlStateManager.disableBlend();
